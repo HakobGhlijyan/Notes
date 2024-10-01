85521c7345063d1e6b39f13fad3797757a6f50b8
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-10-01 16:29:19 +0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Hakob Ghlijyan on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesTests/NotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesTests.swift
+//  NotesTests
+//
+//  Created by Hakob Ghlijyan on 01.10.2024.
+//
+
+import Testing
+@testable import Notes
+
+struct NotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
